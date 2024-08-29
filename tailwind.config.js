7c9a7573034d244930e5a556b148e95ec4590333
Kram-neil <m.jugarap.511752@umindanao.edu.ph>
2024-08-29 20:24:51 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Adjust the path according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
